df1f7005016670286dbf9e833ff303b2365ef911
jbamfo11 <jbamfo@yahoo.com>
2026-08-12 20:36:06 -0400

    Updated index.js

--- CS546.OneOceanAPI/routes/index.js
+++ CS546.OneOceanAPI/routes/index.js
@@ -4,18 +4,15 @@ import communityRoutes from './community.js';
 import usersRoutes from './users.js';
 
 const constructorMethod = (app) => {
-  // Home Page
   app.get('/', (req, res) => {
     res.render('home', { title: 'One Ocean' });
   });
 
-  // Attach Routers
   app.use('/', authRoutes);
   app.use('/', usersRoutes);
   app.use('/beaches', beachesRoutes);
   app.use('/community', communityRoutes);
 
-  // Catch-all 404 handler for unknown routes (Express v5 syntax)
   app.use('/{*splat}', (req, res) => {
     res.status(404).render('error', { error: 'Page Not Found' });
   });
